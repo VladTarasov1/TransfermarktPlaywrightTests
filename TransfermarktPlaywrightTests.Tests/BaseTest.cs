dabe0333dd0115818fdb1e3061a614a8e8e67141
Vlad <vlad1tarasiiiov@gmail.com>
2026-07-08 14:24:39 +0200

    Initial project setup: NUnit + Playwright, smoke test

---
+++ TransfermarktPlaywrightTests.Tests/BaseTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright.NUnit;
+
+namespace TransfermarktPlaywrightTests.Tests;
+
+[TestFixture]
+public class SmokeTest : PageTest
+{
+    [Test]
+    public async Task HomepageLoads()
+    {
+        await Page.GotoAsync("https://www.transfermarkt.com/");
+        await Expect(Page).ToHaveTitleAsync(new System.Text.RegularExpressions.Regex(".*Transfermarkt.*"));
+    }
+}
